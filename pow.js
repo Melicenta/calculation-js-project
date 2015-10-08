1571d5dc55483fcd59385f3b3419062331341fe9
Yelena Denezhnaya <elenamelicenta@gmail.com>
2015-10-08 11:05:50 +0300

    Update pow.js

--- pow.js
+++ pow.js
@@ -1,5 +1,6 @@
 function pow(x, n) {
     var result = 1;
+    /*n is a negative integer number*/
     if (n < 0) {
         n = n * (-1);
         for (var i = 0; i < n; i++) {
@@ -9,20 +10,22 @@ function pow(x, n) {
         }
         console.log("this is n < 0 ",result);
 }
-if (n > 0 && (n ^ 0) === n) {
+    /*n is a positive integer*/
+    if (n > 0 && (n ^ 0) === n) {
     for (var i = 0; i < n; i++) {
         result *= x;
     }
     console.log("this is n > 0 ",result);
 }
-if ((n ^ 0) !== n && x > 0){
+    /*n is a float and positive*/
+    if ((n ^ 0) !== n && x > 0){
     var dec = n*150; //this is to convert decimal fraction into integer and then to take an sqrt from the x
     for (var i = 0; i < n; i++) {
     result = Math.sqrt(x);
     }
     console.log("this is n with floating point ",result);
 }
-return result;
+    return result;
 }
  alert( pow(5,-1));
  alert( pow(5,3));
